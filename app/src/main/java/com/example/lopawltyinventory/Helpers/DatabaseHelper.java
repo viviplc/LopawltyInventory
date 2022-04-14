e784220dc283dbefa1c5969563691a36424de72b
shrey-mehta1 <shreymehta2010@gmail.com>
2022-04-14 19:59:05 -0400

    Fixed error

--- app/src/main/java/com/example/lopawltyinventory/Helpers/DatabaseHelper.java
+++ app/src/main/java/com/example/lopawltyinventory/Helpers/DatabaseHelper.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
-import androidx.room.Delete;
+
 
 import com.example.lopawltyinventory.Models.Product;
 import com.example.lopawltyinventory.R;
